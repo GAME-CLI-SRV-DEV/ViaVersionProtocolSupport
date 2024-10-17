d4c5988659eba7a5e43c2e0e6320424866bc985c
Outfit8TSB <sunnybunnies09@naver.com>
2024-10-17 22:08:49 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 repositories {
     mavenCentral()
-    
+    maven {
+    url = "https://repo.viaversion.com/"
+
+    }
 }
 
 	dependencies {
-	        
+	      compileonly("com.viaversion:ViaVersion:5.0.4")
+              api("net.raphimc:ViaLegacy:5.0.4")
 	}
