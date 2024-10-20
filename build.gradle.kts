4d6196a9ffb720b5b044e4e3a32fdc74b68cb603
Outfit8TSB <sunnybunnies09@naver.com>
2024-10-20 22:56:40 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,6 @@ repositories {
 	dependencies {
 	      compileOnly("com.viaversion:viaversion-common:5.0.4")
               compileOnly("com.viaversion:viabackwards-common:5.0.4")
-              compileOnly("com.viaversion:viarewind-core:3.0.0-SNAPSHOT")
+              compileOnly("com.viaversion:viarewind-common:4.0.3")
               api("net.raphimc:ViaLegacy:3.0.4-SNAPSHOT")
 	}
