bdaa6896cbf0a930f1659862f20280ff04653df6
Outfit8TSB <sunnybunnies09@naver.com>
2024-10-21 22:05:29 +0900

    Update Protocolr1_7_2_5tor1_6_4.java

--- src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/protocol/r1_7_2_5tor1_6_4/Protocolr1_7_2_5tor1_6_4.java
+++ src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/protocol/r1_7_2_5tor1_6_4/Protocolr1_7_2_5tor1_6_4.java
@@ -22,7 +22,7 @@ import static com.viaversion.viaversion.util.ProtocolUtil.packetTypeMap;
 
 // Thanks To ViaBackwards For the Template for ViaLegacy Protocol.
 
-final class Protocol1_6_4To1_7_2_5 extends BackwardsProtocol<ClientboundPackets1_7_2_5, ClientboundPackets1_6_4, ServerboundPackets1_7_2_5, ServerboundPackets1_6_4> {
+final class Protocolr1_7_2_5tor1_6_4 extends BackwardsProtocol<ClientboundPackets1_7_2_5, ClientboundPackets1_6_4, ServerboundPackets1_7_2_5, ServerboundPackets1_6_4> {
 
     // ViaBackwards uses its own mappings and also needs a translatablerewriter for translation mappings
     public static final BackwardsMappingData MAPPINGS = new BackwardsMappingData("1.7.2", "1.6.4", Protocol1_6_4To1_7_2_5.class);
