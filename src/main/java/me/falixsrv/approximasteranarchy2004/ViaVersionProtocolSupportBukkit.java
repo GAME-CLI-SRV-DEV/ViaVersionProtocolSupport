7b351cc056b231048fd94d6422a20335f4ecdd2b
Outfit8TSB <sunnybunnies09@naver.com>
2024-10-22 22:22:03 +0900

    Update ViaVersionProtocolSupportBukkit.java

--- src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupportBukkit.java
+++ src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupportBukkit.java
@@ -17,6 +17,8 @@ import java.io.File;
 
 public class ViaVersionProtocolSupportBukkit extends JavaPlugin {
 
+String filePath = "config.yml";
+File file = new File(filePath);
 
 	@Override
 	public void onLoad() {
@@ -31,6 +33,7 @@ public class ViaVersionProtocolSupportBukkit extends JavaPlugin {
 		logger.severe("║ https://github.com/GAME-CLI-SRV-DEV/ViaVersionProtocolSupport     ");
 		logger.severe("╚══════════════════════════════════════════════════════════════════╝");
             }
+	
 	@Override
 	public void onEnable() {
 	Component message = Component.text()
@@ -42,9 +45,14 @@ Bukkit.getServer().sendMessage(message);
 	}
 
 	public void start() {
-	com.viaversion.viaversion.api.Via.getManager().addEnableListener(() -> this.init(new File(getDataFolder(), "config.yml")));
-	}
 
+        if (!file.exists()) {
+            VVPSConfig.GenerateConfig();
+        } else {
+            System.out.println("File exists.");
+           }
+	}
+      com.viaversion.viaversion.api.Via.getManager().addEnableListener(() -> this.init(new File(getDataFolder(), "vvpsconfig.yml")));
     @Override
     public void disable() {
         System.out.println("Bye!");
