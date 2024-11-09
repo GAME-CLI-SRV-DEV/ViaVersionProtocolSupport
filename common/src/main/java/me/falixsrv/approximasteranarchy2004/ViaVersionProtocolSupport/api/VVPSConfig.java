ead05eeb389f16d6f17af3ad8c6a24f3eb960184
Outfit8TSB <sunnybunnies09@naver.com>
2024-11-09 23:17:44 +0900

    Create VVPSConfig.java

---
+++ common/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/api/VVPSConfig.java
@@ -0,0 +1,21 @@
+package me.falixsrv.approximasteranarchy2004.ViaVersionProtocolSupport.api;
+
+import net.lenni0451.optconfig.ConfigLoader;
+import net.lenni0451.optconfig.annotations.Description;
+import net.lenni0451.optconfig.annotations.OptConfig;
+import net.lenni0451.optconfig.annotations.Option;
+import net.lenni0451.optconfig.provider.ConfigProvider;
+import net.raphimc.viaproxy.util.logging.Logger;
+
+@OptConfig //Required annotation
+public class VVPSConfig {
+
+    @Option("ProtocolSupport") //The name of the option
+    @Description("ran out of ideas") //The description of the option 
+    public static String ProtocolSupport = "i forgor"; //The default value of the option
+
+    @Option("BlockVersion")
+    @Description("ran out of ideas")
+    public static List<String> listOption = List.of("i ran", "Out of ideas");
+
+}
