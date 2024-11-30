b5df18570bbb96110f7ad00f448ca71a26caf8bc
RaphiMC&Lenni0451FTWTurbohaxFTL FTTP AUTTP BFDI OFFICIAL <sunnybunnies09@naver.com>
2024-11-30 16:30:11 +0900

    Update Protocolr1_7_2_5tor1_6_4.java

--- common/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/protocol/r1_7_2_5tor1_6_4/Protocolr1_7_2_5tor1_6_4.java
+++ common/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/protocol/r1_7_2_5tor1_6_4/Protocolr1_7_2_5tor1_6_4.java
@@ -9,8 +9,8 @@ import com.viaversion.viaversion.api.protocol.packet.provider.PacketTypesProvide
 import com.viaversion.viaversion.api.protocol.packet.provider.SimplePacketTypesProvider;
 import com.viaversion.viaversion.data.entity.EntityTrackerBase;
 import net.raphimc.vialegacy.protocol.release.r1_6_4tor1_7_2_5.packet.ClientboundPackets1_6_4;
-import com.viaversion.viarewind.protocol.v1_7_6_10to1_7_2_5.packet.ClientboundPackets1_7_2_5;
-import com.viaversion.viarewind.protocol.v1_7_6_10to1_7_2_5.packet.ServerboundPackets1_7_2_5;
+import net.raphimc.vialegacy.protocol.release.r1_7_2_5tor1_7_6_10.packet.ClientboundPackets1_7_2;
+import net.raphimc.vialegacy.protocol.release.r1_7_2_5tor1_7_6_10.packet.ServerboundPackets1_7_2;
 import net.raphimc.vialegacy.protocol.release.r1_6_4tor1_7_2_5.packet.ServerboundPackets1_6_4;
 import com.viaversion.viaversion.rewriter.ComponentRewriter.ReadType;
 import com.viaversion.viaversion.rewriter.StatisticsRewriter;
@@ -34,7 +34,7 @@ import static com.viaversion.viaversion.util.ProtocolUtil.packetTypeMap;
 
 // Thanks To ViaBackwards For the Template for ViaLegacy Protocol.
 
-public final class Protocolr1_7_2_5tor1_6_4 extends BackwardsProtocol<ClientboundPackets1_7_2_5, ClientboundPackets1_6_4, ServerboundPackets1_7_2_5, ServerboundPackets1_6_4> {
+public final class Protocolr1_7_2_5tor1_6_4 extends BackwardsProtocol<ClientboundPackets1_7_2, ClientboundPackets1_6_4, ServerboundPackets1_7_2, ServerboundPackets1_6_4> {
 
     // ViaBackwards uses its own mappings and also needs a translatablerewriter for translation mappings
     public static final BackwardsMappingData MAPPINGS = new BackwardsMappingData("1.7.2", "1.6.4");
@@ -42,7 +42,7 @@ public final class Protocolr1_7_2_5tor1_6_4 extends BackwardsProtocol<Clientboun
     private final TagRewriter<ClientboundPackets1_7_2_5> tagRewriter = new TagRewriter<>(this);
 
     public Protocolr1_7_2_5tor1_6_4() {
-        super(ClientboundPackets1_7_2_5.class, ClientboundPackets1_6_4.class, ServerboundPackets1_7_2_5.class, ServerboundPackets1_6_4.class);
+        super(ClientboundPackets1_7_2.class, ClientboundPackets1_6_4.class, ServerboundPackets1_7_2.class, ServerboundPackets1_6_4.class);
     }
 
     	@Override
@@ -67,8 +67,8 @@ public final class Protocolr1_7_2_5tor1_6_4 extends BackwardsProtocol<Clientboun
 				});
 			}
 		});
-		this.cancelClientbound(ClientboundPackets1_7_2_5.SET_COMPRESSION); // unused
-		this.registerClientbound(ClientboundPackets1_7_2_5.KEEP_ALIVE, new PacketHandlers() {
+		this.cancelClientbound(ClientboundPackets1_7_2.SET_COMPRESSION); // unused
+		this.registerClientbound(ClientboundPackets1_7_2.KEEP_ALIVE, new PacketHandlers() {
 			@Override
 			public void register() {
 				map(Types.VAR_INT, Types.INT); // id
@@ -83,7 +83,7 @@ public final class Protocolr1_7_2_5tor1_6_4 extends BackwardsProtocol<Clientboun
 			}
 		});
 
-		this.registerServerbound(ServerboundPackets1_7_2_5.KEEP_ALIVE, new PacketHandlers() {
+		this.registerServerbound(ServerboundPackets1_7_2.KEEP_ALIVE, new PacketHandlers() {
 			@Override
 			public void register() {
 				map(Types.INT, Types.VAR_INT); // id
