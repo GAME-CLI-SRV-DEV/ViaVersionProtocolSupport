035f7a05b0a3c97629a8a611c3d1536693c78e22
RaphiMC&Lenni0451FTWTurbohaxFTL FTTP AUTTP BFDI OFFICIAL <sunnybunnies09@naver.com>
2024-12-04 22:43:31 +0900

    Update Protocolr1_7_2_5tor1_6_4.java

--- common/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/protocol/r1_7_2_5tor1_6_4/Protocolr1_7_2_5tor1_6_4.java
+++ common/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/protocol/r1_7_2_5tor1_6_4/Protocolr1_7_2_5tor1_6_4.java
@@ -31,6 +31,7 @@ import com.viaversion.viarewind.ViaRewind;
 import com.viaversion.viaversion.api.Via;
 import com.viaversion.viarewind.protocol.v1_8to1_7_6_10.rewriter.BlockItemPacketRewriter1_8;
 import com.viaversion.viarewind.protocol.v1_8to1_7_6_10.rewriter.EntityPacketRewriter1_8;
+import com.viaversion.viarewind.protocol.v1_8to1_7_6_10.Protocol1_8to1_7_6_10;
 
 // Thanks To ViaBackwards For the Template for ViaLegacy Protocol.
 
