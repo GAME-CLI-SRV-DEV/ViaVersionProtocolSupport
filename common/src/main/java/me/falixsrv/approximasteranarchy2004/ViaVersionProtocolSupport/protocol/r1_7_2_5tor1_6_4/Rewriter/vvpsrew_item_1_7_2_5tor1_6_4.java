33ae66189b8e583d57726588cba635a6c32fb328
RaphiMC&Lenni0451FTWTurbohaxFTL FTTP AUTTP BFDI OFFICIAL <sunnybunnies09@naver.com>
2024-12-13 22:39:45 +0900

    Update vvpsrew_item_1_7_2_5tor1_6_4.java

--- common/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/protocol/r1_7_2_5tor1_6_4/Rewriter/vvpsrew_item_1_7_2_5tor1_6_4.java
+++ common/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/protocol/r1_7_2_5tor1_6_4/Rewriter/vvpsrew_item_1_7_2_5tor1_6_4.java
@@ -12,7 +12,7 @@ import net.raphimc.vialegacy.protocol.release.r1_7_6_10tor1_8.types.Types1_7_6;
 public class vvpsrew_item_1_7_2_5tor1_6_4 extends LegacyItemRewriter<ClientboundPackets1_6_4, ServerboundPackets1_7_2_5, Protocolr1_7_2_5tor1_6_4> {
 
     public vvpsrew_item_1_7_2_5tor1_6_4(final Protocolr1_7_2_5tor1_6_4 Protocol) {
-        super(protocol, "1.6.4", Types1_7_6.ITEM, Types1_7_6.ITEM_ARRAY);
+        super(Protocol, "1.6.4", Types1_7_6.ITEM, Types1_7_6.ITEM_ARRAY);
 
       this.addRemappedItem(355, 26, "Bed Block");
       this.addRemappedItem(33, 34, "Piston Head");
