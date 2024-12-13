da5494f6e99671cde0d5ec7eccf1ee28d0f12771
RaphiMC&Lenni0451FTWTurbohaxFTL FTTP AUTTP BFDI OFFICIAL <sunnybunnies09@naver.com>
2024-12-14 00:27:08 +0900

    Do ►/|| Symbol

--- viaproxy/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/viaproxy/ViaProxyBootstrap.java
+++ viaproxy/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/viaproxy/ViaProxyBootstrap.java
@@ -12,37 +12,45 @@ public class PluginMain extends ViaProxyPlugin implements VVPSPlatform {
     
     @Override
     public void onEnable() {
-       System.out.println("If You Found this Message, Congrats!")
-       System.out.println("This is the VVPS Speaking.");
-       System.out.println("Actually, This Plugin is Beta and will not be released.")
+       System.out.println("[ ⬤ START ] ViaVersionProtocolSupport");
        System.out.println(" ");
-       System.out.println("This Plugin Was Planned When Approximaster Anarchy 2004(main)/AnarchyConnect(connect) Was There,");
-       System.out.println("However, The Server was Shutted Down for, Yes You Guessed it, Ram Cuts. the Main Server Died For Running out of ram.");
-       System.out.println("so The Main Company decided to get a donation.");
-       System.out.println(" ");
-       System.out.println("So Now, this would be a rare, but please donate your Synology NAS Hardware by creating a Draft Donation issue on GAME-CLI-SRV-DEV/ViaProxyConnect or r/approximaster.");
-       System.out.println("Otherwise it will be a tragedy because this christmas would be christmas Without approximaster anarchy server.");
-       System.out.println("goodbye my dear lunchly");
-System.out.println("we will let AnarchyConnect ViaProxy(address_port_version.viaproxy.anarchyconnect.duckdns.org:56114), and AnarchyConnect VIAaaS(address._pPort._vVersion._uUSERNAME._oX.viac9h8o4.anarchyconnect.duckdns.org:21486) on for protesters Againist Villagers654 and Infinite Games Network & Purpur Regime! LET'S OVERTHROW VILLAGERS654 & PURPURMC With Rizz Power of VIAaaS And ViaProxy!");
-System.out.println(" ");
-System.out.println("       /-------------\________/");
-System.out.println("      /  PURPUR SUCKS        /");
-System.out.println("     /    VILLA SUCKS       /");
-System.out.println("    /                      /");  
-System.out.println("   /--------------\_______/");
-System.out.println("  /");
-System.out.println(" /");
+       System.err.println("[ (!) WARN ] ViaVersionProtocolSupport is in beta.")
+       System.err.println("[ (!) WARN ] Especially ViaProxy.")
+       System.err.println("[ (!) WARN ] *** VVPS will Start In 3 Seconds ***")
+       Thread.sleep(3000);
+       System.err.println("If You Found this Message, Congrats!")
+       System.err.println("This is the VVPS Speaking.");
+       System.err.println("Actually, This Plugin is Beta and will not be released until further notice.")
+       System.err.println(" ");
+       System.err.println("This Plugin Was Planned When Approximaster Anarchy 2004(main)/AnarchyConnect(connect) Was There,");
+       System.err.println("However, The Server was Shutted Down for, Yes You Guessed it, Ram Cuts. the Main Server Died For Running out of ram.");
+       System.err.println("so The Main Company decided to get a donation.");
+       System.err.println(" ");
+       System.err.println("So Now, this would be a rare, but please donate your Synology NAS Hardware by creating a Draft Donation issue on GAME-CLI-SRV-DEV/ViaProxyConnect or r/approximaster.");
+       System.err.println("Otherwise it will be a tragedy because this christmas would be christmas Without approximaster anarchy server.");
+       System.err.println("goodbye my dear lunchly");
+System.err.println("we will let AnarchyConnect ViaProxy(address_port_version.viaproxy.anarchyconnect.duckdns.org:56114), and AnarchyConnect VIAaaS(address._pPort._vVersion._uUSERNAME._oX.viac9h8o4.anarchyconnect.duckdns.org:21486) on for protesters Againist Villagers654 and Infinite Games Network & Purpur Regime! LET'S OVERTHROW VILLAGERS654 & PURPURMC With Rizz Power of VIAaaS And ViaProxy!");
+System.err.println(" ");
+System.err.println("       /-------------\________/");
+System.err.println("      /  PURPUR SUCKS        /");
+System.err.println("     /    VILLA SUCKS       /");
+System.err.println("    /                      /");  
+System.err.println("   /--------------\_______/");
+System.err.println("  /");
+System.err.println(" /");
 
 
     }
 
       @EventHandler
     private void onProxyStart(final ProxyStartEvent event) {
+	System.out.println("[ ► PLAY ] Now Loading...")
         this.init();
     }
 
     @EventHandler
     private void onProxyStop(final ProxyStopEvent event) {
+	System.out.println("[ || PAUSE ] pausing...")
         this.disable();
     }
     
@@ -63,13 +71,12 @@ System.out.println(" /");
  
     @Override
     public void disable() { 
-        System.out.println("Proxy Stopped.");
-        ViaProxyPlugin.disable()
+        System.out.println("(PAUSE ||) Proxy Stopped.");
     }
   
     @Override
     public void onDisable() { 
-        System.out.println("ViaVersionProtocolSupport is Shutting Down...");
+        System.out.println("( ■ STOP ) ViaVersionProtocolSupport is Shutting Down...");
     }
 
 }
