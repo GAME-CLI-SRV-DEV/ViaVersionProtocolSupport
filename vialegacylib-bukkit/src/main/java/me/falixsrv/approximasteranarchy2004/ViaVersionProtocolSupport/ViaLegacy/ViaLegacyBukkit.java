b683c007371dd1cf91abd5bbffbd302245e7837d
RaphiMC&Lenni0451FTWTurbohaxFTL FTTP AUTTP BFDI OFFICIAL <sunnybunnies09@naver.com>
2024-12-15 22:11:31 +0900

    Update ViaLegacyBukkit.java

--- vialegacylib-bukkit/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/ViaLegacy/ViaLegacyBukkit.java
+++ vialegacylib-bukkit/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/ViaLegacy/ViaLegacyBukkit.java
@@ -1,12 +1,13 @@
-package me.falixsrv.approximasteranarchy2004.ViaVersionProtocolSupport;
+package me.falixsrv.approximasteranarchy2004.ViaVersionProtocolSupport.ViaLegacy;
 
+import me.falixsrv.approximasteranarchy2004.ViaVersionProtocolSupport.ViaLegacy.ViaLegacyPlugin
 import com.viaversion.viaversion.api.Via;
 
 public class ViaLegacyBukkit extends JavaPlugin {
 
   @Override
   public void onLoad() {
-  Via.getManager().addEnableListener(ViaLegacyPlatformImpl::new);
+  Via.getManager().addEnableListener(ViaLegacyPlugin::new);
      }
   
 }
