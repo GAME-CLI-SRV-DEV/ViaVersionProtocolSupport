cc9c898644e00b4712d34277d3dbc498efe14ac7
RaphiMC&Lenni0451FTWTurbohaxFTL FTTP AUTTP BFDI OFFICIAL <sunnybunnies09@naver.com>
2024-12-15 13:49:00 +0900

    Update build.gradle.kts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 	      compileOnly("com.viaversion:viaversion-common:5.0.4")
               compileOnly("com.viaversion:viabackwards-common:5.0.4")
               compileOnly("com.viaversion:viarewind-common:4.0.3")
-              implementation("net.raphimc:ViaLegacy:3.0.4-SNAPSHOT")
+              compileOnly("net.raphimc:ViaLegacy:3.0.4-SNAPSHOT")
 			  implementation("net.lenni0451:optconfig:1.0.0")
 			  implementation("com.google.guava:guava:33.3.1-jre")
 	}
