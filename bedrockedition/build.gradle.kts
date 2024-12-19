702867915b681e780715c3defc4b274ca9cda105
RaphiMC&Lenni0451FTWTurbohaxFTL FTTP AUTTP BFDI OFFICIAL <sunnybunnies09@naver.com>
2024-12-19 10:10:48 +0900

    업데이트

---
+++ bedrockedition/build.gradle.kts
@@ -0,0 +1,7 @@
+// VVPS Bedrock Only Support Nukkit/PowerNukkit
+// Don't Brag About it does not work on Cloudburst
+// You can use ViaProxyViaVersionProtocolSupport/StarProxy With Cloudburst
+// ViaVersionProtocolSupport: Java to Bedrock Support
+// Powered By ViaLoader
+// Copyright (C) 2015-2024, Approximaster Studios 2004, All Rights Reversed. This Program Is Licensed Under GNU GPL v3 License.
+
