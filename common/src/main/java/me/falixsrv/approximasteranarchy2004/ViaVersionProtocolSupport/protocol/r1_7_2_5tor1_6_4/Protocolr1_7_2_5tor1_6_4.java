baa73d72071368708645ae81123ec015e08f4ffd
RaphiMC&Lenni0451FTWTurbohaxFTL FTTP AUTTP BFDI OFFICIAL <sunnybunnies09@naver.com>
2024-12-21 13:45:58 +0900

    ViaLegacy 확장
    우선 Pull입니다.

--- common/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/protocol/r1_7_2_5tor1_6_4/Protocolr1_7_2_5tor1_6_4.java
+++ common/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/protocol/r1_7_2_5tor1_6_4/Protocolr1_7_2_5tor1_6_4.java
@@ -35,23 +35,15 @@ import me.falixsrv.approximasteranarchy2004.ViaVersionProtocolSupport.protocol.r
 import java.util.concurrent.TimeUnit;
 import com.viaversion.viarewind.protocol.v1_8to1_7_6_10.provider.CompressionHandlerProvider;
 import com.viaversion.viarewind.protocol.v1_8to1_7_6_10.provider.compression.TrackingCompressionHandlerProvider;
+import net.raphimc.vialegacy.protocol.release.r1_7_6_10tor1_8.types.Types1_7_6;
 import com.viaversion.viaversion.api.minecraft.ClientWorld;
 
 // Thanks To ViaBackwards For the Template for ViaLegacy Protocol.
 
-public final class Protocolr1_7_2_5tor1_6_4 extends BackwardsProtocol<ClientboundPackets1_7_2, ClientboundPackets1_6_4, ServerboundPackets1_7_2, ServerboundPackets1_6_4> {
+public final class Protocolr1_7_2_5tor1_6_4 extends StatelessTransitionProtocol<ClientboundPackets1_7_2, ClientboundPackets1_6_4, ServerboundPackets1_7_2, ServerboundPackets1_6_4> {
 
-    // ViaBackwards uses its own mappings and also needs a translatablerewriter for translation mappings
-    public static final RewindMappingData MAPPINGS = new RewindMappingData("1.7.2", "1.6.4");
-    private final TranslatableRewriter<ClientboundPackets1_7_2> translatableRewriter = new TranslatableRewriter<>(this, ReadType.NBT);
-    private final TagRewriter<ClientboundPackets1_7_2> tagRewriter = new TagRewriter<>(this);
     private final vvpsrew_item_1_7_2_5tor1_6_4 itemRewriter = new vvpsrew_item_1_7_2_5tor1_6_4(this);
-   public static final ValueTransformer<String, String> REMOVE_DASHES = new ValueTransformer<>(Types.STRING) {
-		@Override
-		public String transform(PacketWrapper wrapper, String s) {
-			return s.replace("-", "");
-		}
-	};
+
 	
     public Protocolr1_7_2_5tor1_6_4() {
         super(ClientboundPackets1_7_2.class, ClientboundPackets1_6_4.class, ServerboundPackets1_7_2.class, ServerboundPackets1_6_4.class);
@@ -59,49 +51,43 @@ public final class Protocolr1_7_2_5tor1_6_4 extends BackwardsProtocol<Clientboun
 
     	@Override
 	protected void registerPackets() {
-		this.registerClientbound(State.LOGIN, ClientboundLoginPackets.GAME_PROFILE, new PacketHandlers() {
-			@Override
-			public void register() {
-				map(Types.STRING, REMOVE_DASHES); // Uuid
-				map(Types.STRING); // Name
-			}
-		});
+		super.registerPackets();
+	        this.registerClientboundTransition(ClientboundPackets1_7_2.LOGIN,
+                ClientboundPackets1_6_4.LOGIN, new PacketHandlers() {
+                    @Override
+                    public void register() {
+                        map(Types.INT); // entity id
+                        handler(wrapper -> {
+                            wrapper.user().get(PlayerInfoStorage.class).entityId = wrapper.get(Types.INT, 0);
+                            final String terrainType = wrapper.read(Types1_7_6.STRING); // level type
+                            final short gameType = wrapper.read(Types.BYTE); // game mode
+                            final byte dimension = wrapper.read(Types.BYTE); // dimension id
+                            final short difficulty = wrapper.read(Types.BYTE); // difficulty
+                            wrapper.read(Types.BYTE); // world height
+                            final short maxPlayers = wrapper.read(Types.BYTE); // max players
 
-        
- 		this.registerClientbound(State.LOGIN, ClientboundLoginPackets.LOGIN_COMPRESSION, new PacketHandlers() {
-			@Override
-			public void register() {
-				handler(wrapper -> {
-					final int threshold = wrapper.read(Types.VAR_INT);
+                            wrapper.write(Types.UNSIGNED_BYTE, gameType);
+                            wrapper.write(Types.BYTE, dimension);
+                            wrapper.write(Types.UNSIGNED_BYTE, difficulty);
+                            wrapper.write(Types.UNSIGNED_BYTE, maxPlayers);
+                            wrapper.write(Types.STRING, terrainType);
+                        });	
 
-					Via.getManager().getProviders().get(CompressionHandlerProvider.class).onHandleLoginCompressionPacket(wrapper.user(), threshold);
-					wrapper.cancel();
-				});
-			}
-		});
-		this.registerClientbound(ClientboundPackets1_7_2.KEEP_ALIVE, new PacketHandlers() {
-			@Override
-			public void register() {
-				map(Types.VAR_INT, Types.INT); // id
-			}
-		});
-
-		this.registerServerbound(State.LOGIN, ServerboundLoginPackets.ENCRYPTION_KEY, new PacketHandlers() {
-			@Override
-			public void register() {
-				map(Types.SHORT_BYTE_ARRAY, Types.BYTE_ARRAY_PRIMITIVE); // shared secret
-				map(Types.SHORT_BYTE_ARRAY, Types.BYTE_ARRAY_PRIMITIVE); // verification token
-			}
-		});
-
-		this.registerServerbound(ServerboundPackets1_6_4.KEEP_ALIVE, new PacketHandlers() {
-			@Override
-			public void register() {
-				map(Types.INT, Types.VAR_INT); // id
-			}
-		});
 	}
+}, State.LOGIN, (PacketHandler) wrapper -> {
+                    ViaLegacy.getPlatform().getLogger().warning("Server skipped LOGIN state");
+                    final PacketWrapper sharedKey = PacketWrapper.create(ClientboundPackets1_6_4.SHARED_KEY, wrapper.user());
+                    sharedKey.write(Types.SHORT_BYTE_ARRAY, new byte[0]);
+                    sharedKey.write(Types.SHORT_BYTE_ARRAY, new byte[0]);
+                    wrapper.user().get(ProtocolMetadataStorage.class).skipEncryption = true;
+                    sharedKey.send(Protocolr1_6_4Tor1_7_2_5.class, false); // switch to play state
+                    wrapper.user().get(ProtocolMetadataStorage.class).skipEncryption = false;
 
+                    wrapper.setPacketType(ClientboundPackets1_6_4.LOGIN);
+                    wrapper.send(Protocolr1_6_4Tor1_7_2_5.class, false);
+                    wrapper.cancel();
+                }
+        );
 	@Override
 	public void transform(Direction direction, State state, PacketWrapper packetWrapper) throws CancelException {
 		Via.getManager().getProviders().get(CompressionHandlerProvider.class).onTransformPacket(packetWrapper.user());
