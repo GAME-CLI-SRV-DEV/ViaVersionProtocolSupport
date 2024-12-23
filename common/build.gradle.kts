788bf8ac657af784837edab1d126643a1be40501
RaphiMC&Lenni0451FTWTurbohaxFTL FTTP AUTTP BFDI OFFICIAL <sunnybunnies09@naver.com>
2024-12-23 12:07:55 +0900

    Update build.gradle.kts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -11,15 +11,25 @@ repositories {
     url = uri("https://repo.papermc.io/repository/maven-public/")
 
     }
+   maven {
+    url = uri("https://repo.mzga.dev/snapshots/")
+
+   }
+
+    maven {
+    url = uri("https://jitpack.io/")
+    }
 }
 
 	dependencies {
 	      compileOnly("com.viaversion:viaversion-common:5.0.4")
-              compileOnly("com.viaversion:viabackwards-common:5.0.4")
+              compileOnly("com.viaversion:viabackwards-common:5.0.4") 
               compileOnly("com.viaversion:viarewind-common:4.0.3")
-              compileOnly("net.raphimc:ViaLegacy:3.0.4-SNAPSHOT")
-			  implementation("net.lenni0451:optconfig:1.0.0")
-			  implementation("com.google.guava:guava:33.3.1-jre")
+              compileOnly("net.raphimc:ViaLegacy:3.0.6-SNAPSHOT") // main protocol
+              // Fallback Protocol 시작지점
+              implementation("com.github.dirtpowered:BetaProtocollib:1.0-SNAPSHOT")
+              implementation("com.github.Steveice10:MCProtocolLib:c5e4b66")
+              // 
 	}
 
 java {
