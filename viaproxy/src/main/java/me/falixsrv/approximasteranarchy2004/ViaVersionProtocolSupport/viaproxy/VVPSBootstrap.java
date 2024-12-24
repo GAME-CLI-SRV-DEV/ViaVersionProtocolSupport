825a72bb1142ddd802d6b47bb4e523acfc7527ac
RaphiMC&Lenni0451FTWTurbohaxFTL FTTP AUTTP BFDI OFFICIAL <sunnybunnies09@naver.com>
2024-12-25 01:37:18 +0900

    Update VVPSBootstrap.java

--- viaproxy/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/viaproxy/VVPSBootstrap.java
+++ viaproxy/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/viaproxy/VVPSBootstrap.java
@@ -1,6 +1,7 @@
 package me.falixsrv.approximasteranarchy2004.ViaVersionProtocolSupport.viaproxy;
 
 import me.falixsrv.approximasteranarchy2004.ViaVersionProtocolSupport.api.VVPSPlatform;
+import me.falixsrv.approximasteranarchy2004.ViaVersionProtocolSupportMain;
 
 public class VVPSBootstrap implements VVPSPlatform {
 
