c6f5c9008b0e156eec1d0899e74e18047aa92c49
Owner Of Approximaster Studios 2004 <sunnybunnies09@naver.com>
2024-12-30 19:55:31 +0900

    Update Protocolr1_7_2_5tor1_6_4.java
    Rewrite the parts of it

--- common/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/protocol/r1_7_2_5tor1_6_4/Protocolr1_7_2_5tor1_6_4.java
+++ common/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/protocol/r1_7_2_5tor1_6_4/Protocolr1_7_2_5tor1_6_4.java
@@ -1,41 +1,56 @@
 package me.falixsrv.approximasteranarchy2004.ViaVersionProtocolSupport.protocol.r1_7_2_5tor1_6_4;
 
-import net.raphimc.vialegacy.api.LegacyProtocolVersion;
-import com.viaversion.viabackwards.api.data.BackwardsMappingData;
-import com.viaversion.viabackwards.api.rewriters.SoundRewriter;
-import com.viaversion.viabackwards.api.rewriters.TranslatableRewriter;
+import com.viaversion.viaversion.api.Via;
+import com.viaversion.viaversion.api.connection.ProtocolInfo;
 import com.viaversion.viaversion.api.connection.UserConnection;
-import com.viaversion.viaversion.api.protocol.packet.provider.PacketTypesProvider;
-import com.viaversion.viaversion.api.protocol.packet.provider.SimplePacketTypesProvider;
-import com.viaversion.viaversion.data.entity.EntityTrackerBase;
-import net.raphimc.vialegacy.protocol.release.r1_6_4tor1_7_2_5.packet.ClientboundPackets1_6_4;
-import net.raphimc.vialegacy.protocol.release.r1_7_2_5tor1_7_6_10.packet.ClientboundPackets1_7_2;
-import net.raphimc.vialegacy.protocol.release.r1_7_2_5tor1_7_6_10.packet.ServerboundPackets1_7_2;
-import net.raphimc.vialegacy.protocol.release.r1_6_4tor1_7_2_5.packet.ServerboundPackets1_6_4;
-import com.viaversion.viaversion.rewriter.ComponentRewriter.ReadType;
-import com.viaversion.viaversion.rewriter.StatisticsRewriter;
-import com.viaversion.viaversion.rewriter.TagRewriter;
-import com.viaversion.viaversion.protocols.base.ClientboundLoginPackets;
-import com.viaversion.viabackwards.api.BackwardsProtocol;
-import com.viaversion.viarewind.api.data.RewindMappingData;
+import com.viaversion.viaversion.api.minecraft.BlockChangeRecord;
+import com.viaversion.viaversion.api.minecraft.BlockPosition;
+import com.viaversion.viaversion.api.minecraft.ClientWorld;
+import com.viaversion.viaversion.api.minecraft.chunks.Chunk;
+import com.viaversion.viaversion.api.minecraft.entities.EntityTypes1_8;
+import com.viaversion.viaversion.api.minecraft.entitydata.EntityData;
+import com.viaversion.viaversion.api.minecraft.item.DataItem;
+import com.viaversion.viaversion.api.minecraft.item.Item;
 import com.viaversion.viaversion.api.platform.providers.ViaProviders;
-import com.viaversion.viaversion.api.protocol.packet.Direction;
 import com.viaversion.viaversion.api.protocol.packet.PacketWrapper;
 import com.viaversion.viaversion.api.protocol.packet.State;
+import com.viaversion.viaversion.api.protocol.remapper.PacketHandler;
 import com.viaversion.viaversion.api.protocol.remapper.PacketHandlers;
-import com.viaversion.viaversion.api.protocol.remapper.ValueTransformer;
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import com.viaversion.viaversion.api.type.Types;
-import com.viaversion.viarewind.protocol.v1_8to1_7_6_10.storage.*;
-import com.viaversion.viaversion.protocols.base.ServerboundLoginPackets;
-import com.viaversion.viaversion.exception.CancelException;
-import com.viaversion.viarewind.protocol.v1_8to1_7_6_10.task.WorldBorderUpdateTask;
-import com.viaversion.viarewind.ViaRewind;
-import com.viaversion.viaversion.api.Via;
+import com.viaversion.viaversion.libs.fastutil.ints.Int2IntMap;
+import com.viaversion.viaversion.libs.fastutil.objects.Object2IntMap;
+import com.viaversion.viaversion.libs.fastutil.objects.Object2IntOpenHashMap;
+import com.viaversion.viaversion.libs.gson.JsonObject;
+import com.viaversion.viaversion.protocols.base.*;
+import com.viaversion.viaversion.protocols.base.v1_7.ClientboundBaseProtocol1_7;
+import com.viaversion.viaversion.protocols.v1_8to1_9.packet.ClientboundPackets1_8;
+import com.viaversion.viaversion.util.IdAndData;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+import me.falixsrv.approximasteranarchy2004.ViaVersionProtocolSupport.ViaVersionProtocolSupportMain;
+import net.raphimc.vialegacy.api.protocol.StatelessTransitionProtocol;
+import net.raphimc.vialegacy.api.splitter.PreNettySplitter;
+import net.raphimc.vialegacy.api.util.PacketUtil;
+import net.raphimc.vialegacy.protocol.release.r1_6_4tor1_7_2_5.packet.ClientboundPackets1_6_4;
+import net.raphimc.vialegacy.protocol.release.r1_6_4tor1_7_2_5.packet.ServerboundPackets1_6_4;
+import net.raphimc.vialegacy.protocol.release.r1_6_4tor1_7_2_5.provider.EncryptionProvider;
+import net.raphimc.vialegacy.protocol.release.r1_6_4tor1_7_2_5.rewriter.ItemRewriter;
+import net.raphimc.vialegacy.protocol.release.r1_6_4tor1_7_2_5.rewriter.SoundRewriter;
+import net.raphimc.vialegacy.protocol.release.r1_6_4tor1_7_2_5.rewriter.StatisticRewriter;
+import net.raphimc.vialegacy.protocol.release.r1_6_4tor1_7_2_5.rewriter.TextRewriter;
+import net.raphimc.vialegacy.protocol.release.r1_6_4tor1_7_2_5.storage.*;
+import net.raphimc.vialegacy.protocol.release.r1_6_4tor1_7_2_5.types.EntityDataTypes1_6_4;
+import net.raphimc.vialegacy.protocol.release.r1_6_4tor1_7_2_5.types.Types1_6_4;
+import net.raphimc.vialegacy.protocol.release.r1_7_2_5tor1_7_6_10.packet.ClientboundPackets1_7_2;
+import net.raphimc.vialegacy.protocol.release.r1_7_2_5tor1_7_6_10.packet.ServerboundPackets1_7_2;
+import net.raphimc.vialegacy.protocol.release.r1_7_6_10tor1_8.Protocolr1_7_6_10Tor1_8;
+import net.raphimc.vialegacy.protocol.release.r1_7_6_10tor1_8.model.GameProfile;
+import net.raphimc.vialegacy.protocol.release.r1_7_6_10tor1_8.provider.GameProfileFetcher;
+import net.raphimc.vialegacy.protocol.release.r1_7_6_10tor1_8.types.EntityDataTypes1_7_6;
+import net.raphimc.vialegacy.protocol.release.r1_7_6_10tor1_8.types.Types1_7_6;
 import me.falixsrv.approximasteranarchy2004.ViaVersionProtocolSupport.protocol.r1_7_2_5tor1_6_4.Rewriter.vvpsrew_item_1_7_2_5tor1_6_4; // from ViaLegacy, Modified for a backward compat
-import java.util.concurrent.TimeUnit;
-import com.viaversion.viarewind.protocol.v1_8to1_7_6_10.provider.CompressionHandlerProvider;
-import com.viaversion.viarewind.protocol.v1_8to1_7_6_10.provider.compression.TrackingCompressionHandlerProvider;
-import com.viaversion.viaversion.api.minecraft.ClientWorld;
 
 // Thanks To ViaBackwards For the Template for ViaLegacy Protocol.
 
@@ -87,10 +102,11 @@ public final class Protocolr1_7_2_5tor1_6_4 extends BackwardsProtocol<Clientboun
                     sharedKey.write(Types.SHORT_BYTE_ARRAY, new byte[0]);
                     sharedKey.write(Types.SHORT_BYTE_ARRAY, new byte[0]);
                     wrapper.user().get(ProtocolMetadataStorage.class).skipEncryption = true;
+		    ViaVersionProtocolSupportMain.getPlatform().getLogger().severe("Sending Shared Key... (WARN: I SWEAR IT WILL NOT WORK)");
                     sharedKey.send(Protocolr1_6_4Tor1_7_2_5.class, false); // switch to play state
                     wrapper.user().get(ProtocolMetadataStorage.class).skipEncryption = false;
 
-                    wrapper.setPacketType(ClientboundPackets1_6_4.LOGIN);
+                    wrapper.setPacketType(ClientboundPackets1_7_2.LOGIN);
                     wrapper.send(Protocolr1_6_4Tor1_7_2_5.class, false);
                     wrapper.cancel();
                 }
@@ -155,7 +171,7 @@ public final class Protocolr1_7_2_5tor1_6_4 extends BackwardsProtocol<Clientboun
             setVelocityToZero.send(Protocolr1_6_4Tor1_7_2_5.class);
             wrapper.cancel();
         });
-        this.registerClientbound(ClientboundPackets1_7_2.MOVE_PLAYER_POS, ClientboundPackets1_6_4.PLAYER_POSITION, wrapper -> {
+        this.registerClientbound(ClientboundPackets1_7_2.PLAYER_POSITION, ClientboundPackets1_6_4.MOVE_PLAYER_POS, wrapper -> {
             final PlayerInfoStorage playerInfoStorage = wrapper.user().get(PlayerInfoStorage.class);
             final boolean supportsFlags = wrapper.user().getProtocolInfo().protocolVersion().newerThanOrEqualTo(ProtocolVersion.v1_8);
 
@@ -176,7 +192,7 @@ public final class Protocolr1_7_2_5tor1_6_4 extends BackwardsProtocol<Clientboun
                 wrapper.passthrough(Types.BOOLEAN); // onGround
             }
         });
-        this.registerClientbound(ClientboundPackets1_6_4.MOVE_PLAYER_ROT, ClientboundPackets1_7_2.PLAYER_POSITION, wrapper -> {
+        this.registerClientbound(ClientboundPackets1_7_2.PLAYER_POSITION, ClientboundPackets1_6_4.MOVE_PLAYER_ROT, wrapper -> {
             final PlayerInfoStorage playerInfoStorage = wrapper.user().get(PlayerInfoStorage.class);
             final boolean supportsFlags = wrapper.user().getProtocolInfo().protocolVersion().newerThanOrEqualTo(ProtocolVersion.v1_8);
 
