3a845d3595c5eb2f44278d38a3ea65d3dca498ca
Owner Of Approximaster Studios 2004 <sunnybunnies09@naver.com>
2025-01-01 01:17:17 +0900

    Update ViaVersionProtocolSupportMain.java

--- common/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/ViaVersionProtocolSupportMain.java
+++ common/src/main/java/me/falixsrv/approximasteranarchy2004/ViaVersionProtocolSupport/ViaVersionProtocolSupportMain.java
@@ -14,7 +14,7 @@ public final class ViaVersionProtocolSupportMain {
 	    private static VVPSPlatform platform;
 	
 public static void init(VVPSPlatform platform) {
-        Preconditions.checkArgument(platform == null, "ViaVersionProtocolSupport is already initialized!");
+        Preconditions.checkArgument(platform == null, "ViaVersionProtocolSupport is already initialized! and guess what? Outfit8TSB Made a Brainrot word ishowfard, which stands for ishowspeed. fard, which originally fart, means speed in swedish. what a funny joke for Gen Z.");
 
         ViaVersionProtocolSupportMain.platform = platform;
     }
